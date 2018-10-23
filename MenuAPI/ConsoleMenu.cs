8ac6e61b42b746419b8258c23f749af65a5aba0c
Falk <katzen.alphabet@gmx.net>
2018-10-23 18:57:45 +0200

    Initial Commit

---
+++ MenuAPI/ConsoleMenu.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuAPI
+{
+    public class ConsoleMenu
+    {
+        protected string[] _entries;
+        protected string _title;
+        protected string _seperator;
+        protected char _selector;
+
+        /// <summary>
+        /// Constructs a Menu Object
+        /// </summary>
+        /// <param name="entries">A String array containing all menu items</param>
+        /// <param name="selector">Character used for indicating current selection</param>
+        /// <param name="seperator">Sets a custom string for list seperation. menu Items exactly matching the seperator, will not be selectable</param>
+        public ConsoleMenu(string[] entries, char selector = '>', string seperator = "")
+        {
+            _entries = (string[])entries.Clone();
+            _seperator = seperator;
+            _selector = selector;
+            _title = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+        }
+        /// <summary>
+        /// Constructs a Menu Object
+        /// </summary>
+        /// <param name="entries">A String array containing all menu items</param>
+        /// <param name="title">Sets a custom menu title</param>
+        /// <param name="selector">Character used for indicating current selection</param>
+        /// <param name="seperator">Sets a custom string for list seperation. menu Items exactly matching the seperator, will not be selectable</param>
+        public ConsoleMenu(string[] entries, string title, char selector = '>', string seperator = "")
+        {
+            _entries = (string[])entries.Clone();
+            _seperator = seperator;
+            _selector = selector;
+            _title = title;
+        }
+
+        /// <summary>
+        /// Shows the menu and allows user to select something from the menu
+        /// </summary>
+        /// <returns>The index of the selected item with just the selectable items</returns>
+        public virtual int Show()
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.WriteLine(_title);
+            string underline = "";
+            for (int i = 0; i < _title.Length; i++)
+            {
+                underline += "-";
+            }
+            Console.WriteLine(underline + "\n");
+
+            Console.WriteLine(_selector + " " + _entries[0]);
+            for (int i = 1; i < _entries.Length; i++)
+            {
+                Console.WriteLine("  " + _entries[i]);
+            }
+
+            ConsoleKey input = 0;
+            int cSelect = 0;
+
+            do
+            {
+                input = Console.ReadKey(true).Key;
+
+                if (input == ConsoleKey.DownArrow)
+                {
+                    Console.SetCursorPosition(0, 3 + cSelect);
+                    Console.Write(" ");
+
+                    cSelect++;
+                    if (cSelect >= _entries.Length)
+                    {
+                        cSelect = 0;
+                    }
+                    if (_entries[cSelect] == _seperator)
+                    {
+                        cSelect++;
+                    }
+
+                    Console.SetCursorPosition(0, 3 + cSelect);
+                    Console.Write(_selector);
+                }
+                else if (input == ConsoleKey.UpArrow)
+                {
+                    Console.SetCursorPosition(0, 3 + cSelect);
+                    Console.Write(" ");
+
+                    cSelect--;
+                    if (cSelect < 0)
+                    {
+                        cSelect = _entries.Length - 1;
+                    }
+                    if (_entries[cSelect] == _seperator)
+                    {
+                        cSelect--;
+                    }
+
+                    Console.SetCursorPosition(0, 3 + cSelect);
+                    Console.Write(_selector);
+                }
+            } while (input != ConsoleKey.Enter && input != ConsoleKey.Escape);
+
+            int tmpSelect = -1;
+
+            if (input == ConsoleKey.Enter)
+            {
+                tmpSelect = cSelect;
+                for (int i = 0; i < cSelect; i++)
+                {
+                    if (_entries[i] == _seperator)
+                    {
+                        tmpSelect -= 1;
+                    }
+                }
+            }
+
+            Console.CursorVisible = true;
+            return tmpSelect;
+        }
+    }
+
+    public class ConsoleMenuRowed : ConsoleMenu
+    {
+        protected int _lines;
+        protected int _seperation;
+
+        /// <summary>
+        /// Constructs a Rowed Menu Object
+        /// </summary>
+        /// <param name="entries">A String array containing all menu items</param>
+        /// <param name="lines">Amount of lines, before the menu jumps to the next row</param>
+        /// <param name="seperation">Amount of space, between the indiviual rows</param>
+        /// <param name="selector">Character used for indicating current selection</param>
+        /// <param name="seperator">Sets a custom string for list seperation. menu Items exactly matching the seperator, will not be selectable</param>
+        public ConsoleMenuRowed(string[] entries, int lines, int seperation = 2, char selector = '>', string seperator = "") : base(entries, selector, seperator)
+        {
+            _lines = lines;
+            _seperation = seperation;
+        }
+        /// <summary>
+        /// Constructs a Rowed Menu Object
+        /// </summary>
+        /// <param name="entries">A String array containing all menu items</param>
+        /// <param name="title">Sets a custom menu title</param>
+        /// <param name="lines">Amount of lines, before the menu jumps to the next row</param>
+        /// <param name="seperation">Amount of space, between the indiviual rows</param>
+        /// <param name="selector">Character used for indicating current selection</param>
+        /// <param name="seperator">Sets a custom string for list seperation. menu Items exactly matching the seperator, will not be selectable</param>
+        public ConsoleMenuRowed(string[] entries, string title, int lines, int seperation = 2, char selector = '>', string seperator = "") : base(entries, title, selector, seperator)
+        {
+            _lines = lines;
+            _seperation = seperation;
+        }
+
+        /// <summary>
+        /// Shows the menu and allows user to select something from the menu
+        /// </summary>
+        /// <returns>The index of the selected item with just the selectable items</returns>
+        public override int Show()
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.WriteLine(_title);
+            string underline = "";
+            for (int i = 0; i < _title.Length; i++)
+            {
+                underline += "-";
+            }
+            Console.WriteLine(underline + "\n");
+
+            int rows = _entries.Length / _lines;
+            int[] rowWidths = new int[rows];
+            int cX = 0;
+
+            for (int x = 0; x < rows; x++)
+            {
+                int cWidth = 0;
+                for (int y = 0; y < _lines; y++)
+                {
+                    Console.SetCursorPosition(cX, y + 3);
+                    if (x + y == 0)
+                    {
+                        Console.Write(_selector + " " + _entries[0]);
+                    }
+                    else
+                    {
+                        Console.Write("  " + _entries[y + (x * rows)]);
+                    }
+
+                    if (_entries[y + (x*rows)].Length > cWidth)
+                    {
+                        cWidth = _entries[y + (x * rows)].Length;
+                    }
+                }
+                cX += cWidth + _seperation + 2;
+                rowWidths[x] = cWidth + 2;
+            }
+
+            ConsoleKey input = 0;
+            int cSelect = 0;
+
+            do
+            {
+                int xPos;
+                input = Console.ReadKey(true).Key;
+
+                if (input == ConsoleKey.DownArrow)
+                {
+                    drawCursor(cSelect, ref rowWidths, ' ');
+
+                    cSelect++;
+                    if (cSelect >= _entries.Length)
+                    {
+                        cSelect = 0;
+                    }
+                    if (_entries[cSelect] == _seperator)
+                    {
+                        cSelect++;
+                    }
+
+                    drawCursor(cSelect, ref rowWidths, _selector);
+                }
+                else if (input == ConsoleKey.UpArrow)
+                {
+                    drawCursor(cSelect, ref rowWidths, ' ');
+
+                    cSelect--;
+                    if (cSelect < 0)
+                    {
+                        cSelect = _entries.Length - 1;
+                    }
+                    if (_entries[cSelect] == _seperator)
+                    {
+                        cSelect--;
+                    }
+
+                    drawCursor(cSelect, ref rowWidths, _selector);
+                }
+            } while (input != ConsoleKey.Enter && input != ConsoleKey.Escape);
+
+            int tmpSelect = -1;
+
+            if (input == ConsoleKey.Enter)
+            {
+                tmpSelect = cSelect;
+                for (int i = 0; i < cSelect; i++)
+                {
+                    if (_entries[i] == _seperator)
+                    {
+                        tmpSelect -= 1;
+                    }
+                }
+            }
+
+            Console.CursorVisible = true;
+            return tmpSelect;
+        }
+
+        /// <summary>
+        /// Helper-Function to draw the cursor at the appropriate position
+        /// </summary>
+        /// <param name="cPos">Position inside the entries array</param>
+        /// <param name="rowWidths">Widths of all used rows</param>
+        /// <param name="symbol">The symbol that's supposed to be drawn</param>
+        void drawCursor(int cPos, ref int[] rowWidths, char symbol)
+        {
+            int xPos = 0;
+            for (int i = 0; i < cPos / _lines; i++)
+            {
+                xPos = rowWidths[i] + _seperation;
+            }
+            Console.SetCursorPosition(xPos, 3 + cPos % _lines);
+            Console.Write(symbol);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # Console Menu API
 A Console Menu API for C#.NET
+
+## Installation
+To istall the API, you add it as a Reference in VS and then you can use this API.
+
+## Usage
+The API Uses the MenuAPI Namespace and has the following Objects:
+- ConsoleMenu
+  - The default Console Menu. All Entries are listed under each other
+- ConsoleMenuRowed
+  - Modifies the Console Meu to show up in multiple rows. The row-count is calculated dynamically and the Hight of the menu is set by you.
+
+### ConsoleMenu
+Constructor:
+```
+	ConsoleMenu(string[] entries, char selector = '>', string seperator = "")
+	ConsoleMenu(string[] entries, string title, char selector = '>', string seperator = "")
+```
+- entries
+  - Is an array of all menu options, that should show up.
+- title
+  - specifies the application/menu title
+- selector
+  - specifies the character to indicate the currently selected menu option
+- sperator
+  - specifies the seperator menu item (aka entries with the exact matching name are not selectable)
+
+Other public functions:
+```
+	int Show()
+```
+- Show()
+  - Shows the menu and halts the current thread, until the user presses Enter or Escape
+  - returns whatever entry the user selected or -1 if the user pressed Escape.
+    - returned Index is the Index of the entries array **without** seperators
+
+### ConsoleMenuRowed
+Constructor:
+```
+	ConsoleMenuRowed(string[] entries, int lines, int seperation = 2, char selector = '>', string seperator = "")
+	ConsoleMenuRowed(string[] entries, string title, int lines, int seperation = 2, char selector = '>', string seperator = "")
+```
+- entries
+  - Is an array of all menu options, that should show up.
+- title
+  - specifies the application/menu title
+- lines
+  - specifies how many lines should be available for the entries, before wrapping back to the top in the next row
+- seperation
+  - specifies how many empty rows should be between individual rows of entries
+- selector
+  - specifies the character to indicate the currently selected menu option
+- sperator
+  - specifies the seperator menu item (aka entries with the exact matching name are not selectable)
+
+Other public functions:
+```
+	int Show()
+```
+- Show()
+  - Shows the menu and halts the current thread, until the user presses Enter or Escape
+  - returns whatever entry the user selected or -1 if the user pressed Escape.
+    - returned Index is the Index of the entries array **without** seperators
